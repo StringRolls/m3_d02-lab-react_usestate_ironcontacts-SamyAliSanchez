72f7c18d0da6283b5bd6693b56035ee506f59c62
Samy Ali <samy.alisanchez91@gmail.com>
2022-05-24 18:30:11 +0200

    iteration 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsArray from "./contacts.json";
+import { useState } from "react";
+
+const firstFive = contactsArray.slice(5, 10);
 
 function App() {
+  const [contacts, setContacts] = useState(firstFive);
+
+  const addContact = () => {
+    let randomIndex = Math.floor(Math.random() * contactsArray.length);
+    const random = contactsArray[randomIndex];
+    if (contacts.includes(random)) {
+    } else {
+      setContacts(contacts.concat(random).reverse());
+    }
+  };
+
+  const popularitySort = () => {
+    const sortedContacts = contacts
+      .sort((a, b) => b.popularity - a.popularity)
+      .slice();
+    setContacts(sortedContacts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ironcarts</h1>
+      <div>
+        <button onClick={addContact}>Add random contact</button>
+        <button onClick={popularitySort}>Sort by popularity</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, idx) => {
+            return (
+              <tr key={idx}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt=""
+                    className="table"
+                    style={{ height: "250px", width: "auto" }}
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
